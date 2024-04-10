f1d7534e6f35232ed731d57bb973987959b5c87d
Cesar Iparrea <cesariparrea@hotmail.com>
2024-04-10 18:18:34 -0400

    All pages completed missing help page

--- src/screens/favoritesPage/FavoritesPage.jsx
+++ src/screens/favoritesPage/FavoritesPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import './FavoritesPage.css'
 import Navbar from '../../components/navbar/Navbar.jsx'
+import Event from '../../components/event/Event.jsx'
 
 
 function FavoritesPage() {
   return (
     <div className='favoritesPage'>
       <Navbar show="noshow"/>
-      favorites page
+      <Event />
+      <Event />
+      <Event />
+      <Event />
+      <Event />
+      <Event />
+      <Event />
+      <Event />
+      <Event />
 
     </div>
   )
