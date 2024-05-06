a0684e2d08f2e0ae46bdba583c9b6a8ccfcc8f19
Ambre Tate <ambretate@gmail.com>
2024-05-06 11:30:36 -0400

    Services for searching events, function for search bar component

--- src/components/serachBar/SearchBar.jsx
+++ src/components/serachBar/SearchBar.jsx
@@ -1,31 +1,47 @@
-import React from 'react'
-import './SearchBar.css'
+import React, { useState, useEffect } from "react";
+import "./SearchBar.css";
 import { useNavigate } from "react-router-dom";
+import { getSearchedEvents } from "../../Services/events.js";
 
+function SearchBar({ show }) {
+  const [searchedEvents, setSearchedEvents] = useState([]);
+  const [form, setForm] = useState("");
 
-function SearchBar({show}) {
-  const navigate = useNavigate();
-
-  const handleChange = (event) =>{
-    event.preventDefault()
+  const handleChange = (event) => {
+    setForm(event.target.value);
   };
 
-  const handleSearchSubmit = async (event) =>{
-      navigate("/search");
-  }
-  
+  console.log(form)
+
+  const handleSearchSubmit = async (event) => {
+    event.preventDefault();
+    const searchedEventsFetched = await getSearchedEvents(form);
+    console.log(searchedEventsFetched);
+    return setSearchedEvents(searchedEventsFetched);
+  };
+
+  // useEffect(() => {
+  //   const fetchSearchedEvents = async () => {
+  //     const searchedEventsFetched = await getSearchedEvents(form);
+  //     console.log(searchedEventsFetched);
+  //     return setSearchedEvents(searchedEventsFetched);
+  //   }
+  // })
+
   return (
-    <div className='searchBar' id={show}>
-        <form 
-        onSubmit={handleSearchSubmit}
-        className='searchBarForm'>
-            <input 
-            onChange={handleChange}
-            className='searchBarInput' type='search' placeholder="Search..." ></input>
-            <div className='searchBarIcon'></div>
-        </form>
+    <div className="searchBar" id={show}>
+      <form onSubmit={handleSearchSubmit} className="searchBarForm">
+        <input
+          type="text"
+          name="search"
+          placeholder="Search..."
+          onChange={handleChange}
+          className="searchBarInput"
+        ></input>
+        <div className="searchBarIcon"></div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
